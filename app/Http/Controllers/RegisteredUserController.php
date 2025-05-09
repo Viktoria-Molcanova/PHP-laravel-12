2bf12a05ecd7819d64255ceb7c606a9df3b94073
Viktoria <molcanovaviktoria660@gmail.com>
2025-05-09 20:03:44 +0700

    дз

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
-class RegisteredUserController
+class RegisteredUserController extends Controller
 {
     public function store(Request $request): \Illuminate\Http\JsonResponse
     {
